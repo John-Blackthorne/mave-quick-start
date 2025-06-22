a46fe5dad325a593dc19fe64c5142bbf265bed27
Polina Dobrovolskaia <dobrovolskaiapolina@gmail.com>
2025-06-22 09:03:35 +0200

    Endringer i Application.java

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -2,13 +2,15 @@ package clinic.programming.training;
 
 import java.util.ArrayList;
 import java.util.List;
-
+import org.apache.commons.lang3.StringUtils;
 
 public class Application {
 
-public int countWords(String words){
-    return 1;
-}
+    public int countWords(String words){
+        String[] separateWords = StringUtils.split(words, ' ');
+        return (separateWords == null) ? 0 : separateWords.length;
+    }
+    
 
     public void greet(){
         List<String> greetings = new ArrayList<>();
@@ -28,5 +30,9 @@ public int countWords(String words){
         System.out.println ("Starting Application");
         Application app = new Application();
         app.greet();
+
+
+        int count = app.countWords("I have four words");
+        System.out.println("Word Count: " + count);
     }
 }
